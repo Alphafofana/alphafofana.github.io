a12b90bdadeef0d3356481f8a47683e7eee3e92f
alphaf <>
2020-08-04 19:02:20 +0200

    Updates

---
+++ precache-manifest.34482a3fd2fe4525cf8aa8a9a10dcd7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67126ccf4ab0ab6d01b0ae9e22b38f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d05e8447d3eb6fbd3fc",
+    "url": "/static/css/main.b528ca75.chunk.css"
+  },
+  {
+    "revision": "1998bd15fb5780369e68",
+    "url": "/static/js/2.0ad2dcc4.chunk.js"
+  },
+  {
+    "revision": "33068e4a9f5685e528be5e17653bc86f",
+    "url": "/static/js/2.0ad2dcc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d05e8447d3eb6fbd3fc",
+    "url": "/static/js/main.7f587140.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "f35f5006f97864352c4eb0b6e74ab8e1",
+    "url": "/static/media/profile.f35f5006.jpg"
+  }
+]);
